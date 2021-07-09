946ffbe551c498dc8608c6470ed2057366c8cd65
Hoang Truong <62192868+0h59p@users.noreply.github.com>
2021-07-09 15:45:08 +0700

    remakea

---
+++ app/src/main/java/com/shashank/sony/fancygifdialoglibrary/MainActivity.java
@@ -0,0 +1,30 @@
+package com.shashank.sony.fancygifdialoglibrary;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shashank.sony.fancygifdialoglib.FancyGifDialog;
+import com.shashank.sony.fancygifdialoglib.FancyGifDialogListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+
+
+
+
+}
